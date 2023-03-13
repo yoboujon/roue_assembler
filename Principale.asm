1016a9456a76b8b4c06f3f79f8f477b42fef1a73
Yohan Boujon <yoboujon@gmail.com>
2023-03-13 11:05:25 +0100

    Allumage et la pas lumière avec des registres

--- Principale.asm
+++ Principale.asm
@@ -47,8 +47,25 @@ main  	PROC
 ;*******************************************************************************
 
 		
-		MOV R0,#0
+		MOV R0,#0;
 		BL Init_Cible;
+		; SET
+		LDR R12,=0x40010C00
+		MOV R5,#(0x01 << 10)
+		STRH R5,[R12,#0x10]	;On stocke la variable R5 à l'adresse 0x0X40010C10
+		; RESET
+		MOV R5,#(0x01 << 10)
+		STRH R5,[R12,#0x14]	;On stocke la variable R5 à l'adresse 0x0X40010C10
+		
+		; ALLUMER LA LED
+		LDR R5,[R12,#0x0C]		;Valeur à l'adresse de l'output
+		ORR R5, R5,#(0x01 << 10)	;OU LOGIQUE pour calculer la valeur a mettre dans l'output
+		STRH R5,[R12,#0x0C]			;Etat du port B (R5) stocké dans l'output 
+		
+		;ETEINDRE LA LED
+		LDR R5,[R12,#0x0C]		;Valeur à l'adresse de l'output
+		AND R5, R5,#~(0x01 << 10)	;OU LOGIQUE pour calculer la valeur a mettre dans l'output
+		STRH R5,[R12,#0x0C]			;Etat du port B (R5) stocké dans l'output 
 		
 		B .			 ; boucle inifinie terminale...
 
