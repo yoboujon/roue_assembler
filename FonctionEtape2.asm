42ad6bb6062d940b61d24d552ca99b04c4939ec1
Yohan Boujon <yo.boujon@gmail.com>
2023-03-26 00:41:52 +0100

    Added Set_X and Reset_X functions with args (pin placement). Added SCLK and SIN global constants

--- FonctionEtape2.asm
+++ FonctionEtape2.asm
@@ -30,7 +30,9 @@
 	 AREA  MesDonnees, data, readwrite
 ;**************************************************************************
 
- 
+SCLK 		EQU 5
+SIN1 		EQU 7
+
 Barette1 	DCB 0,1,0
 			DCB 0,1,0
 			DCB 0,1,0
@@ -58,9 +60,42 @@ Set_SCLK	PROC
 		BX LR						;Retour
 	
 	ENDP
+		
+Set_X	PROC
+		PUSH {R7}					;On stocke R7 dans SP
+		MOV R7,SP					;R7 récupère l'adresse du SP
+		PUSH {R0-R4}				;On stocke R0 à R4 dans SP
+		LDRB R0,[R7,#4]				;Argument 1 dans R0
+		MOV R4, #1					;*******
+		LSL R0, R4, R0				;1<<Arg
+		LDR R1,=GPIOBASEA			;R1 -> Adresse de GPIOA
+		LDRH R2,[R1,#OffsetOutput]	;Valeur à l'adresse d'ODR : R2 = GPIOA->ODR
+		ORR R2, R2, R0				;similaire à GPIOA->ODR |= (1<<Arg)
+		STRH R2,[R1,#OffsetOutput]	;Etat du port B (R5) stocké dans ODR
+		POP{R0-R4,R7}				;Déchargement de la pile
+		BX LR						;Retour
+	
+	ENDP
+		
+Reset_X	PROC
+		PUSH {R7}					;On stocke R7 dans SP
+		MOV R7,SP					;R7 récupère l'adresse du SP
+		PUSH {R0-R4}				;On stocke R0 à R4 dans SP
+		LDRB R0,[R7,#4]				;Argument 1 dans R0
+		MOV R4, #1					;*******
+		LSL R0, R4, R0				;1<<Arg
+		MVN R0, R0					;~(1<<Arg)
+		LDR R1,=GPIOBASEA			;R1 -> Adresse de GPIOA
+		LDRH R2,[R1,#OffsetOutput]	;Valeur à l'adresse d'ODR : R2 = GPIOA->ODR
+		AND R2, R2, R0				;similaire à GPIOA->ODR &= ~(1<<Arg)
+		STRH R2,[R1,#OffsetOutput]	;Etat du port B (R5) stocké dans ODR
+		POP{R0-R4,R7}				;Déchargement de la pile
+		BX LR						;Retour
+	
+	ENDP
 
 Reset_SCLK	PROC
-		PUSH {R0-R2}				;On stocke R0 à R2
+		PUSH {R0-R2}				;On stocke R0 à R2 dans SP
 		LDR R1,=GPIOBASEA			;R1 -> Adresse de GPIOA
 		LDRH R2,[R1,#OffsetOutput]	;Valeur à l'adresse d'ODR : R2 = GPIOA->ODR
 		AND R2, R2,#~(0x01 << 5)	;similaire à GPIOA->ODR &= ~(1<<5)
@@ -75,7 +110,9 @@ Reset_SCLK	PROC
 ;R8 = ValCourante[NBLed]
 ;****************************************************************************
 DriverGlobal	PROC
-		BL Set_SCLK;
+		MOV R0, #SCLK	;************
+		PUSH {R0}		;Set_X(SCLK)
+		BL Set_X;		;************
 		MOV R6, #0;
 WHILE_NBLED
 		;Pour NbLed = 1 à 48
