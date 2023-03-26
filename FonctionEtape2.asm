68717e2a2d5d8cac6bc6766c92efa7e55b1c07b3
Yohan Boujon <yo.boujon@gmail.com>
2023-03-26 17:59:06 +0200

    Modified Barette1, fixed Set_X and Reset_X memory overflow. Added the rest of the algorithm to send LED data.

--- FonctionEtape2.asm
+++ FonctionEtape2.asm
@@ -32,14 +32,24 @@
 
 SCLK 		EQU 5
 SIN1 		EQU 7
-
-Barette1 	DCB 0,1,0
-			DCB 0,1,0
-			DCB 0,1,0
-			DCB 0,1,0
-			DCB 0,1,0
-			DCB 0,1,0
-			DCB 0,1,0
+	
+PF			DCD (1<<31)
+Barette1 	DCB 0xff,0,0
+			DCB 0,0xff,0
+			DCB 0,0,0xff
+			DCB 0xff,0,0
+			DCB 0xff,0xff,0
+			DCB 0xff,0xff,0xff
+			DCB 0xff,0,0
+			DCB 0,0xff,0
+			DCB 0,0,0xff
+			DCB 0xff,0,0
+			DCB 0xff,0xff,0
+			DCB 0xff,0xff,0xff
+			DCB 0xff,0,0
+			DCB 0xff,0xff,0
+			DCB 0xff,0xff,0xff
+			DCB 0x0f,0xff,0x00
 			
 
 
@@ -62,34 +72,28 @@ Set_SCLK	PROC
 	ENDP
 		
 Set_X	PROC
-		PUSH {R7}					;On stocke R7 dans SP
-		MOV R7,SP					;R7 récupère l'adresse du SP
-		PUSH {R0-R4}				;On stocke R0 à R4 dans SP
-		LDRB R0,[R7,#4]				;Argument 1 dans R0
-		MOV R4, #1					;*******
-		LSL R0, R4, R0				;1<<Arg
+		PUSH {R1,R2}				;On stocke R0 à R4 dans SP
+		MOV R1, #1					;*******
+		LSL R0, R1, R0				;1<<Arg
 		LDR R1,=GPIOBASEA			;R1 -> Adresse de GPIOA
 		LDRH R2,[R1,#OffsetOutput]	;Valeur à l'adresse d'ODR : R2 = GPIOA->ODR
 		ORR R2, R2, R0				;similaire à GPIOA->ODR |= (1<<Arg)
 		STRH R2,[R1,#OffsetOutput]	;Etat du port B (R5) stocké dans ODR
-		POP{R0-R4,R7}				;Déchargement de la pile
+		POP{R1,R2}					;Déchargement de la pile
 		BX LR						;Retour
 	
 	ENDP
 		
 Reset_X	PROC
-		PUSH {R7}					;On stocke R7 dans SP
-		MOV R7,SP					;R7 récupère l'adresse du SP
-		PUSH {R0-R4}				;On stocke R0 à R4 dans SP
-		LDRB R0,[R7,#4]				;Argument 1 dans R0
-		MOV R4, #1					;*******
-		LSL R0, R4, R0				;1<<Arg
+		PUSH {R1,R2}				;On stocke R0 à R4 dans SP
+		MOV R1, #1					;*******
+		LSL R0, R1, R0				;1<<Arg
 		MVN R0, R0					;~(1<<Arg)
 		LDR R1,=GPIOBASEA			;R1 -> Adresse de GPIOA
 		LDRH R2,[R1,#OffsetOutput]	;Valeur à l'adresse d'ODR : R2 = GPIOA->ODR
 		AND R2, R2, R0				;similaire à GPIOA->ODR &= ~(1<<Arg)
 		STRH R2,[R1,#OffsetOutput]	;Etat du port B (R5) stocké dans ODR
-		POP{R0-R4,R7}				;Déchargement de la pile
+		POP{R1,R2}					;Déchargement de la pile
 		BX LR						;Retour
 	
 	ENDP
@@ -105,30 +109,52 @@ Reset_SCLK	PROC
 	ENDP
 		
 ;****************************************************************************
-;R6 = NBLed
-;R7 = *ValCourante
-;R8 = ValCourante[NBLed]
+;R1 = *ValCourante
+;R2 = NBLed (i)
+;R3 = ValCourante[i]
 ;****************************************************************************
 DriverGlobal	PROC
-		MOV R0, #SCLK	;************
-		PUSH {R0}		;Set_X(SCLK)
-		BL Set_X;		;************
-		MOV R6, #0;
-WHILE_NBLED
-		;Pour NbLed = 1 à 48
-		LDR R7,=Barette1	;On recupère l'adresse de base
-		LDRB R8,[R7,R6]		;R8 = ValCourante[...NBLed]
+		MOV R0, #SCLK	;Argument SCLK
+		BL Set_X;		;Set_X(SCLK)
+		LDR R1,=Barette1;On recupère l'adresse de base
 		
-		LSL R8,#24			;ValCourante[NBLed]<<24
+		MOV R2, #0;			;*************************
+WHILE_NBLED					;for(int i=0;i<48;i++)
+		LDRB R3,[R1,R2]		;ValCourante[i]
+		LSL R3,#24			;ValCourante[i]<<24
 		
-		ADD R6, R6, #1		;On incrémente R6
-		CMP R6, #47			; SI R6==47 alors on arrête la boucle
+		LDR R0,=PF
+		LDR R5,[R0,#0]	;R5 = (1<<31)
+		MOV R4, #0		;*************************
+WHILE_NBBIT				;for(int j=0;j<12;j++)
+		MOV R0, #SCLK		;Argument SCLK
+		BL Reset_X;			;Reset_X(SCLK)
+		MOV R0, #SIN1		;Argument SIN1
+		AND R6,R3,R5		;ValCourante[i] &= (1<<31) (<- PF)
+		CMP R6,R5			;if(PF == 1)
+		BEQ PoidFortOKIF	;{ Set_X(SIN1) }
+		BL Reset_X;			;else { Reset_X(SIN1) }
+PoidFortOKJUMP				;Fin Si
+		LSL R3,#1			;ValeurCourante[i]<<1
+		MOV R0, #SCLK		;Argument SCLK
+		BL Set_X;			;Set_X(SCLK)
+		ADD R4, R4, #1		;On incrémente NBBit
+		CMP R4, #11			;SI NBBIT==11 alors on arrête la boucle
+		BNE WHILE_NBBIT
+		
+		ADD R2, R2, #1		;On incrémente NBLed
+		CMP R2, #47			;SI NBLED==47 alors on arrête la boucle
 		BNE WHILE_NBLED
 		
-		MOV R0, #SCLK	;************
-		PUSH {R0}		;Reset_X(SCLK)
-		BL Reset_X;		;************
+		MOV R0, #SCLK	;Argument SCLK
+		BL Reset_X;		;Reset_X(SCLK)
+		;LDR R0,=		;DataSend <- 0
 		B .				;while(1)
+		
+PoidFortOKIF
+	BL Set_X			;Set_X(SCLK)
+	B PoidFortOKJUMP	;After Reset8X
+	
 	ENDP
 
 ;**************************************************************************
--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # But du projet
-Le but sera de faire fonctionner diverses LEDS à l'aide d'un STM32 et tout cela en langage Assembler
+Le but sera de faire fonctionner diverses LEDS à l'aide d'un STM32 et tout cela en langage Assembler.
+# Fonctionnalitées
+## Fonctions
+|Nom|Argument(s)|Retour|Description|
+|---|---|---|---|
+|Set_X|**1** - R0 : PINAX||Pour un output donné, met à 1 ce dernier.|
+|Reset_X|**1** - R0 : PINAX||Pour un output donné, force à 0 ce dernier.|
+|DriverGlobal|||Pour une Barette de LED donnée, envoie les signaux demandés|
+
+---
+Chaque fonction prendra des arguments de R0 à R3 (avec R3 étant une référence au tas si le besoin d'argument est supérieur à 3). Le renvoi se fait sur R0.
+
+## Main
+
+Le main pour l'instant ne fait qu'appeler DriverGlobal.
+
+## Variables globales
+
+- SCLK *(5)* et SIN1 *(7)* sont des variables globales permettant avec la fonction Set/Reset_X de définir l'état de sortie d'une pin X. 
+- PF *(1<<31)* est le poids fort, comme il n'est pas possible d'utiliser l'instruction **MOV** avec des nombres supérieurs à 1 octet, il est préférable d'utiliser une variable globale avec cette valeur.
+- Barette1 (16\*3 valeurs), tableau contenant pour chaque LED *(16)*, le niveau RVB.
+
+## Chronogramme
+
+Voici le premier chronogramme observable avec les états de SCLK et SIN1. Aucun test matériel n'a encore été réalisé :
+![SIN SCLK Graph](assets/graph_complete.png)
+
 # Réaliser un code assembler à partir de C
 Comme vous le savez le code en langage C peut être compilé puis récupéré en assembler. C'est justement ici une solution que j'ai trouvé pour mieux comprendre différents principes, ou si certaines instructions ne me paraissent pas clair.
 Bien évidemment le but du projet n'est pas de recopier bêtement du code que le compilateur peut réaliser, mais de comprendre et de voir comment faire différents algorithmes en Assembler.
@@ -48,16 +74,16 @@ Nous obtenons le résultat suivant :
 
 ```assembly
 00000000 <invert>:
-   0:e1e00000 mvnr0, r0
-   4:e12fff1e bxlr
+   0:e1e00000 mvn   r0, r0
+   4:e12fff1e bx    lr
 
 00000008 <set>:
-   8:e3a01001 movr1, #1
-   c:e59f3010 ldrr3, [pc, #16]@ 24 <add+0x1c>
-  10:e5932000 ldrr2, [r3]
-  14:e1d230b5 ldrhr3, [r2, #5]
-  18:e1c33011 bicr3, r3, r1, lsl r0
-  1c:e1c230b5 strhr3, [r2, #5]
-  20:e12fff1e bxlr
-  24:00000000 andeqr0, r0, r0
+   8:e3a01001 mov   r1, #1
+   c:e59f3010 ldr   r3, [pc, #16]@ 24 <add+0x1c>
+  10:e5932000 ldr   r2, [r3]
+  14:e1d230b5 ldrh  r3, [r2, #5]
+  18:e1c33011 bic   r3, r3, r1, lsl r0
+  1c:e1c230b5 strh  r3, [r2, #5]
+  20:e12fff1e bx    lr
+  24:00000000 andeq r0, r0, r0
 ```
